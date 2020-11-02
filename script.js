276b78c492f35b6afaba8cfa01845740b821a49d
Nadirah Jama <Nadirahjama@gmail.com>
2020-11-01 19:46:00 -0600

    fixed and updated information

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 $(document).ready(function(){
 
+    $("#currentDay").text(moment().format("MMMM Do YY, H:mm a"))
+
+    let timeId = parseInt($(this).attr("id").split("-")[1])
+
+    console.log(timeId)
+
+   $(".row").each(function(index){
+    
+    let currentTime = moment().format("H:mm a")
+
+    if (currentTime === timeId){
+        $(this).addClass(".past")
+    }
+
+    
 
 
-  
+   })
 
+    
 
+   
 
-
-
+ 
 
 })
+
+    
+
+
+        
+
