c9bb61b2458909300410cfbaf5c4715818d5fd93
Nadirah Jama <Nadirahjama@gmail.com>
2020-11-02 19:16:36 -0600

    final project completed

--- script.js
+++ script.js
@@ -1,39 +1,64 @@
 $(document).ready(function(){
 
-    $("#currentDay").text(moment().format("MMMM Do YY, H:mm a"))
+    $("#currentDay").text(moment().format("MMMM Do YY"))
 
-    let timeId = $(".hour").each(function(i){
-        $(this).attr("value")
+    $("#hour9").val(localStorage.getItem("hour.9"))
+    $("#hour10").val(localStorage.getItem("hour.10"))
+    $("#hour11").val(localStorage.getItem("hour.11"))
+    $("#hour12").val(localStorage.getItem("hour.12"))
+    $("#hour13").val(localStorage.getItem("hour.13"))
+    $("#hour14").val(localStorage.getItem("hour.14"))
+    $("#hour15").val(localStorage.getItem("hour.15"))
+    $("#hour16").val(localStorage.getItem("hour.16"))
+    $("#hour17").val(localStorage.getItem("hour.17"))
 
-       
+    $(".saveBtn").on("click", function () {
+        let timeBlock = $(this).parent().attr("id");
+        let textArea = $(this).siblings(".description").val()
+        localStorage.setItem(timeBlock, textArea);
+        console.log()
+    });
 
+
+
+  function trackTime (){
+    let currentTime = moment().hour();
+    $(".time-block").each(function(){
+        let allTime = parseInt($(this).attr("id").split(".")[1]);
+
+        if(allTime < currentTime){
+            $(this).addClass("past")
+
+        }if (allTime === currentTime){
+            $(this).addClass("present")
+            $(this).removeClass("past")
+        }if (allTime > currentTime){
+            $(this).addClass("future")
+            $(this).removeClass("past")
+            $(this).removeClass("past")
+        }
     })
 
-    
-    
+  }
+  trackTime()
+}) 
 
-    
 
-   $(".row").each(function(index){
-    
-    let currentTime = moment().format("H:mm a") 
 
-    if(timeId === currentTime) {
-        $(this).addClass(".present")
-    }
+   
    
 
-    console.log(timeId)
 
 
-   })
+
+
 
     
 
   
  
 
-})
+
 
     
 
